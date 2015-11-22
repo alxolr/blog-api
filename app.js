4cbc46d5eefb7d5d4ef65e40005b2bccf0ae3297
Alexandru Olaru <alxolr@gmail.com>
2015-11-22 10:22:02 +0200

    Added all the need files

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
 app.use('/', require('./routes/index'));
-app.use('/', require('./routes/admin'));
+app.use('/', require('./routes/users'));
 app.use(function (req, res, next) {
 	res.status(404).render('404', {title: 'Resource not found'});
 });
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
-	res.render('index', {title: 'Homepage', authorised: false});
-});
-
-router.get('/articles', function (req, res) {
-	res.render('articles', {title: 'Articles'});
+	res.render('index', {title: 'Homepage'});
 });
 
 module.exports = router;
