899069ebcaf6f6b31f4f4a1afcca5adb47cd30a7
Alexandru Olaru <alxolr@gmail.com>
2016-01-05 22:33:22 +0200

    Fixed the todo list

--- routes/todos.js
+++ routes/todos.js
@@ -31,10 +31,17 @@ module.exports = function(app, passport) {
 
 		Todo.find({
 			user_id: req.user.id,
-			done_at: {
-				$gte: start,
-				$lt: end
-			}
+			$or: [{
+				done_at: {
+					$exists: false
+				}
+			}, {
+				done_at: {
+					$gte: start,
+					$lt: end
+				}
+			}]
+
 		}, function(err, todos) {
 			if (err) res.send(err);
 			res.json(todos);
@@ -49,9 +56,27 @@ module.exports = function(app, passport) {
 		}, function(err, todo) {
 			if (err) res.send(err);
 
-			Todo.find(function(err, todos) {
-				if (err) res.send(err);
+			var start = new Date();
+			start.setHours(0, 0, 0, 0);
 
+			var end = new Date();
+			end.setHours(23, 59, 59, 999);
+
+			Todo.find({
+				user_id: req.user.id,
+				$or: [{
+					done_at: {
+						$exists: false
+					}
+				}, {
+					done_at: {
+						$gte: start,
+						$lt: end
+					}
+				}]
+
+			}, function(err, todos) {
+				if (err) res.send(err);
 				res.json(todos);
 			});
 		});
@@ -68,9 +93,27 @@ module.exports = function(app, passport) {
 		}, function(err, done) {
 			if (err) res.send(err);
 
-			Todo.find(function(err, todos) {
-				if (err) res.send(err);
+			var start = new Date();
+			start.setHours(0, 0, 0, 0);
 
+			var end = new Date();
+			end.setHours(23, 59, 59, 999);
+
+			Todo.find({
+				user_id: req.user.id,
+				$or: [{
+					done_at: {
+						$exists: false
+					}
+				}, {
+					done_at: {
+						$gte: start,
+						$lt: end
+					}
+				}]
+
+			}, function(err, todos) {
+				if (err) res.send(err);
 				res.json(todos);
 			});
 		});
