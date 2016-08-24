d81924a0c388288310dbacf3b5cb7fc5bf06e997
Alexandru Olaru <aolaru@gmail.com>
2016-08-24 16:39:30 +0300

    Refactored the test extracted method

--- test/user-routes.test.js
+++ test/user-routes.test.js
@@ -22,24 +22,31 @@
             });
         });
 
-        function assertOk(err, body, isSuccess, message, next) {
+        const user = {
+            email: "dummy@user.com",
+            password: "dummypass"
+        };
+
+        const assertOk = (err, body, isSuccess, message, next) => {
             assert.equal(err, null);
 
             let json = JSON.parse(body);
             assert.equal(json.success, isSuccess);
             assert.equal(json.message, message);
             next();
-        }
+        };
+
+        const generateUser = (cb) => {
+            request.post(resource, {
+                form: user
+            }, cb);
+        };
+
 
         // Create user tests
         describe('Create user', () => {
             it('Should create a user if email and password provided', (done) => {
-                request.post(resource, {
-                    form: {
-                        email: 'alxolr@gmail.com',
-                        password: 'test'
-                    }
-                }, (err, res, body) => {
+                generateUser((err, res, body) => {
                     assertOk(err, body, true, utils.messages.USER_CREATED_SUCCESS, done);
                 });
             });
@@ -47,29 +54,22 @@
 
         // Login User Tests
         describe('Login user', () => {
+            let url = `${resource}/login`;
             it(`Should return "${utils.messages.EMAIL_NO_MATCH}" for invalid email login.`, (done) => {
-                request.post(resource + '/login', {
-                    form: {
-                        email: 'johny@bravo.com',
-                        password: '1'
-                    }
+                request.post(url, {
+                    form: user
                 }, (err, res, body) => {
                     assertOk(err, body, false, utils.messages.EMAIL_NO_MATCH, done);
                 });
             });
 
             it(`Should return "${utils.messages.PASSWORD_NO_MATCH}" given an invalid password for login`, (done) => {
-                request.post(resource, {
-                    form: {
-                        email: 'johny@bravo.com',
-                        password: '1'
-                    }
-                }, (err, res, body) => {
+                generateUser((err, res, body) => {
                     assert.equal(err, null);
-                    request.post(`${resource}/login`, {
+                    request.post(url, {
                         form: {
-                            email: 'johny@bravo.com',
-                            password: 'test'
+                            email: user.email,
+                            password: '1'
                         }
                     }, (err, res, body) => {
                         assertOk(err, body, false, utils.messages.PASSWORD_NO_MATCH, done);
@@ -77,20 +77,11 @@
                 });
             });
 
-
             it('Should return a "token" when the login is succesful', (done) => {
-                request.post(resource, {
-                    form: {
-                        email: 'dony@trouble.com',
-                        password: '123'
-                    }
-                }, (err, result, body) => {
+                generateUser((err, res, body) => {
                     assert.equal(err, null);
-                    request.post(resource + '/login', {
-                        form: {
-                            email: 'dony@trouble.com',
-                            password: '123'
-                        }
+                    request.post(url, {
+                        form: user
                     }, (err, res, body) => {
                         let result = JSON.parse(body);
                         assert.equal(typeof result.token, 'string');
@@ -102,17 +93,11 @@
 
         describe('Update User', () => {
             it(`Should return "${utils.messages.INVALID_TOKEN}" when updating user without token`, (done) => {
-                //create a user
-                request.post(resource, {
-                    form: {
-                        email: 'johny@bravo.com',
-                        password: '1'
-                    }
-                }, (err, res, body) => {
+                generateUser((err, res, body) => {
                     assert.equal(err, null);
                     let result = JSON.parse(body),
                         token = result.token,
-                        userId = '' + result.user._id;
+                        userId = result.user._id;
                     request.put(`${resource}/${userId}`, {
                         form: {
                             email: 'jack@bravo.com'
@@ -124,12 +109,7 @@
             });
 
             it(`Should return "${utils.messages.USER_UPDATED_SUCCESS}" for valid data and token provided`, (done) => {
-                request.post(resource, {
-                    form: {
-                        email: 'johny@bravo.com',
-                        password: '1'
-                    }
-                }, (err, res, body) => {
+                generateUser((err, res, body) => {
                     assert.equal(err, null);
                     let result = JSON.parse(body),
                         token = result.token,
@@ -150,12 +130,7 @@
         describe('Get User', () => {
             it(`Should return "${utils.messages.INVALID_TOKEN}" when token not provided`, done => {
                 //create a user
-                request.post(resource, {
-                    form: {
-                        email: "domy@jigly.com",
-                        password: 'domy1'
-                    }
-                }, (err, res, body) => {
+                generateUser((err, res, body) => {
                     assert.equal(err, null);
                     let json = JSON.parse(body),
                         userId = json.user._id;
@@ -169,12 +144,7 @@
 
             it(`Should return the requested user given the valid token and userId`, done => {
                 //create a user
-                request.post(resource, {
-                    form: {
-                        email: "domy@jigly.com",
-                        password: 'domy1'
-                    }
-                }, (err, res, body) => {
+                generateUser((err, res, body) => {
                     assert.equal(err, null);
                     let json = JSON.parse(body),
                         userId = json.user._id,
@@ -194,15 +164,10 @@
             });
 
             it(`Should return "${utils.messages.INVALID_MONGO_ID}" given an invalid User id`, done => {
-                request.post(resource, {
-                    form: {
-                        email: 'andron2@gmail.com',
-                        password: 'sarugon'
-                    }
-                }, (err, res, body) => {
+                generateUser((err, res, body) => {
                     assert.equal(err, null);
-                    let json = JSON.parse(body);
 
+                    let json = JSON.parse(body);
                     request(`${resource}/1`, {
                         form: {
                             token: json.token
