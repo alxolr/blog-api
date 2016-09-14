29a5b0a396d816454e3d2675c4f63fda64092fef
Alexandru Olaru <aolaru@gmail.com>
2016-09-14 14:13:51 +0300

    Removed the not needed logs

--- server.js
+++ server.js
@@ -15,7 +15,6 @@
     mongoose.Promise = global.Promise;
     mongoose.connect(config.database, (err) => {
         assert.equal(err, null);
-        console.log(`Successfuly connected to ${config.database}`);
     });
 
     // parse application/x-www-form-urlencoded
@@ -26,14 +25,13 @@
     // parse application/json
     app.use(bodyParser.json());
     app.use('/images', express.static('images'));
+    app.use('/coverage', express.static('coverage/lcov-report/'));
 
     app.use('/api/v1/users', userRoutes);
     app.use('/api/v1/articles', articleRoutes);
     app.use('/api/v1/articles/', commentRoutes);
 
-    app.listen(config.port, () => {
-        console.log(`Server is running at http://localhost:${config.port}/`);
-    });
+    app.listen(config.port);
 
     module.exports = app;
 })();
