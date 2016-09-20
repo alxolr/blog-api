c6229e8e0d5c553f695037318565fb10ee7b6391
Alexandru Olaru <aolaru@gmail.com>
2016-09-20 17:31:12 +0300

    removed unused values in messages

--- services/utils.js
+++ services/utils.js
@@ -30,20 +30,12 @@
             EMAIL_REQUIRED: "User should provide a valid email address.",
             INVALID_CREDENTIALS: "The provided email and password does not match.",
             USER_DUPLICATE: "User already registered in the system.",
-            USER_CREATED_SUCCESS: "User was succesfully created.",
-            USER_UPDATED_SUCCESS: "The user was successfully updated",
-            USER_DELETED_SUCCESS: "The user was successfully deleted",
-            USER_LOGGEDIN_SUCCESS: "The user was successfully logged in.",
             USER_NOT_FOUND: "Requested user not found.",
             TOKEN_NOT_PROVIDED: "Access denied. Please provide a token.",
             TOKEN_EXPIRED: "Access denied. Provided token or invalid or expired",
             TOKEN_HIGHJACKED: "Access denied. Provided token was highjacked",
             MONGOID_INVALID: "Provided ID is not a valid one.",
-            ARTICLE_CREATE_SUCCESS: "Article was successfully created.",
-            ARTICLE_UPDATE_SUCCESS: "Article was successfully updated.",
-            ARTICLE_DELETE_SUCCESS: "Article was successfully deleted.",
             ARTICLE_NOT_FOUND: "Requested article was not found",
-            COMMENT_CREATE_SUCCESS: "The comment has been succesfully added",
             COMMENT_CREATE_FAIL: "The comment was not been succesfull"
         }
     };
