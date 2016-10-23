c221c43457333f0a02a3b0ca05568556b043b52b
Alexandru Olaru <alxolr@gmail.com>
2016-10-23 21:29:41 +0300

    Refactored the article routes to conform to eslint

--- routes/article.js
+++ routes/article.js
@@ -1,232 +1,230 @@
 (() => {
-    "use strict";
-    const router = require('express').Router(),
-        mw = require('../middlewares/middlewares'),
-        Article = require('../models/article'),
-        utils = require('../services/utils'),
-        fs = require('fs'),
-        multer = require('multer'),
-        uploadFile = require('../services/upload-file'),
-        upload = multer({
-            dest: '/tmp/'
-        });
-    let imgDirectory = __dirname + '/../images';
+  'use strict'
+  const router = require('express').Router()
+  const mw = require('../middlewares/middlewares')
+  const Article = require('../models/article')
+  const utils = require('../services/utils')
+  const multer = require('multer')
+  const path = require('path')
+  const uploadFile = require('../services/upload-file')
+  const upload = multer({
+    dest: '/tmp/'
+  })
 
+  let imgDirectory = path.resolve('images/')
 
-    const createArticle = (req, res) => {
-        let article = new Article(req.body);
-        article.author = {
-            _id: req.decoded._doc._id,
-            name: req.decoded._doc.name,
-            surname: req.decoded._doc.surname
-        };
-        if (req.file !== undefined) {
-            uploadFile(req.file, imgDirectory).then(() => {
-                article.image = `/images/${req.file.originalname}`;
-                saveArticle(article, res);
-            }).catch(handleErrors(res));
-        } else {
-            saveArticle(article, res);
+  const createArticle = (req, res) => {
+    let article = new Article(req.body)
+    article.author = {
+      _id: req.decoded._doc._id,
+      name: req.decoded._doc.name,
+      surname: req.decoded._doc.surname
+    }
+    if (req.file !== undefined) {
+      uploadFile(req.file, imgDirectory).then(() => {
+        article.image = `/images/${req.file.originalname}`
+        saveArticle(article, res)
+      }).catch(handleErrors(res))
+    } else {
+      saveArticle(article, res)
+    }
+  }
+
+  const getArticleBy = (property) => {
+    return function (req, res) {
+      let query = {
+        deleted_at: {
+          '$exists': false
         }
-    };
-
-    const getArticleBy = (property) => {
-        return function (req, res) {
-            let query = {
-                deleted_at: {
-                    '$exists': false
-                }
-            };
-            if (property === "slug") {
-                query.slug = req.params[property];
-            } else {
-                query._id = req.params[property];
-            }
-            Article
-                .findOne(query)
-                .then((article) => {
-                    if (article) {
-                        res.json(article);
-                    } else {
-                        res.status(404).json({
-                            error: utils.messages.ARTICLE_NOT_FOUND
-                        });
-                    }
-                })
-                .catch(handleErrors(res));
-        };
-    };
-
-    const updateArticle = (req, res) => {
-        if (req.body.title !== undefined) {
-            req.body.slug = utils.slugify(req.body.title);
-        }
-
-        if (req.body._id !== undefined) delete req.body._id;
-
-        if (req.file !== undefined) {
-            uploadFile(req.file, imgDirectory)
-                .then(() => {
-                    req.body.image = `/images/${req.file.originalname}`;
-                    Article.update({
-                        _id: req.params.articleId
-                    }, {
-                        '$set': req.body
-                    }).then(handleSuccess, handleErrors(res));
-                })
-                .catch(handleErrors(res));
-        } else {
-            Article.update({
-                    _id: req.params.articleId
-                }, {
-                    '$set': req.body
-                })
-                .then(handleSuccess)
-                .catch(handleErrors(res));
-        }
-
-        function handleSuccess(result) {
-            Article.findOne({
-                    _id: req.params.articleId
-                })
-                .then(article => res.json(article))
-                .catch(handleErrors(res));
-        }
-    };
-
-    const softDeleteArticle = (req, res) => {
-        Article
-            .update({
-                _id: req.params.articleId
-            }, {
-                $set: {
-                    deleted_at: new Date()
-                }
+      }
+      if (property === 'slug') {
+        query.slug = req.params[property]
+      } else {
+        query._id = req.params[property]
+      }
+      Article
+        .findOne(query)
+        .then((article) => {
+          if (article) {
+            res.json(article)
+          } else {
+            res.status(404).json({
+              error: utils.messages.ARTICLE_NOT_FOUND
             })
-            .then((result) => {
-                //204 No Content
-                res.status(204).end();
-            })
-            .catch(handleErrors(res));
-    };
+          }
+        })
+        .catch(handleErrors(res))
+    }
+  }
 
-    function handleErrors(res) {
-        return (err) => {
-            res.status(400).json({
-                error: err
-            });
-        };
+  const updateArticle = (req, res) => {
+    if (req.body.title !== undefined) {
+      req.body.slug = utils.slugify(req.body.title)
     }
 
-    function saveArticle(article, res) {
-        article.save((err) => {
-            if (!err) {
-                res.json(article);
-            } else {
-                handleErrors(res);
-            }
-        });
+    if (req.body._id !== undefined) delete req.body._id
+
+    if (req.file !== undefined) {
+      uploadFile(req.file, imgDirectory)
+        .then(() => {
+          req.body.image = `/images/${req.file.originalname}`
+          Article.update({
+            _id: req.params.articleId
+          }, {
+            '$set': req.body
+          }).then(handleSuccess, handleErrors(res))
+        })
+        .catch(handleErrors(res))
+    } else {
+      Article.update({
+        _id: req.params.articleId
+      }, {
+        '$set': req.body
+      }).then(handleSuccess)
+        .catch(handleErrors(res))
     }
 
+    function handleSuccess (result) {
+      Article.findOne({
+        _id: req.params.articleId
+      })
+        .then(article => res.json(article))
+        .catch(handleErrors(res))
+    }
+  }
 
-    router.get('', (req, res) => {
-        res.json([]);
-    });
+  const softDeleteArticle = (req, res) => {
+    Article
+      .update({
+        _id: req.params.articleId
+      }, {
+        $set: {
+          deleted_at: new Date()
+        }
+      })
+      .then((result) => {
+        // 204 No Content
+        res.status(204).end()
+      })
+      .catch(handleErrors(res))
+  }
 
+  function handleErrors (res) {
+    return (err) => {
+      res.status(400).json({
+        error: err
+      })
+    }
+  }
+
+  function saveArticle (article, res) {
+    article.save((err) => {
+      if (!err) {
+        res.json(article)
+      } else {
+        handleErrors(res)
+      }
+    })
+  }
+
+  router.get('', (req, res) => {
+    res.json([])
+  })
+
+  /**
+   * @apiDefine ArticleSuccess
+   *
+   * @apiSuccess {Boolean} success Returns if the article was succesfully created.
+   * @apiSuccess {Object} article Returns the newly created article
+   * @apiSuccess {String} message Returns the success message.
+   */
+
+  /**
+   * @api {post} /api/v1/articles
+   * @apiName createArticle
+   * @apiVersion 1.0.0
+   * @apiGroup Article
+   * @apiPermission USER
+   *
+   * @apiParam {String} title Article title
+   * @apiParam {String} body Article body
+   * @apiParam {String} [image] Image for the article
+   * @apiParam {String} token Access Token
+   *
+   * @apiParamExample {json} Request-Example:
+   *     {
+   *       "title": "Interesting awesome title",
+   *       "body": "Here will be the body of the article",
+   *       "token": "anexampleoftoken"
+   *     }
+   *
+   * @apiUse ArticleSuccess
+   */
+  router.post('/', upload.single('image'), mw.isAuthenticated, createArticle)
+
+  router.route('/:articleId([0-9a-f]{24})')
     /**
-     * @apiDefine ArticleSuccess
-     * 
-     * @apiSuccess {Boolean} success Returns if the article was succesfully created.
-     * @apiSuccess {Object} article Returns the newly created article
-     * @apiSuccess {String} message Returns the success message. 
-     */
-
-    /**
-     * @api {post} /api/v1/articles
-     * @apiName createArticle
+     * @api {put} /api/v1/articles/{articleId}
+     * @apiName updateArticle
      * @apiVersion 1.0.0
      * @apiGroup Article
      * @apiPermission USER
-     * 
-     * @apiParam {String} title Article title
-     * @apiParam {String} body Article body
+     * @apiDescription Is updating the requested article which is defined by :articleId mongodb _id equivalent.
+     *
+     * @apiParam {String} [title] Article title
+     * @apiParam {String} [body] Article body
      * @apiParam {String} [image] Image for the article
      * @apiParam {String} token Access Token
-     * 
+     *
      * @apiParamExample {json} Request-Example:
      *     {
      *       "title": "Interesting awesome title",
      *       "body": "Here will be the body of the article",
      *       "token": "anexampleoftoken"
      *     }
-     * 
+     *
      * @apiUse ArticleSuccess
      */
-    router.post('/', upload.single('image'), mw.isAuthenticated, createArticle);
+    .put(upload.single('image'), mw.isAuthenticated, mw.isAdminOrArticleAuthor, updateArticle)
+    /**
+     * @api {get} /api/v1/articles/{articleId}
+     * @apiName getArticleByArticleId
+     * @apiVersion 1.0.0
+     * @apiGroup Article
+     * @apiPermission GUEST
+     * @apiDescription Is returning the request article by articleId.
+     *
+     * @apiSuccess {Object} article The requested article object.
+     * @apiSuccess {Boolean} success The status of the transaction.
+     */
+    .get(getArticleBy('articleId'))
+    /**
+     * @api {delete} /api/v1/articles/{articleId}
+     * @apiName softDeleteArticle
+     * @apiVersion 1.0.0
+     * @apiGroup Article
+     * @apiPermission ARTICLE_AUTHOR
+     *
+     * @apiDescription It will softdelete the article. Article will not be seen anywhere in the system till the deleted_at field removed.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *     HTTP/1.1 204 No Content
+     */
+    .delete(mw.isAuthenticated, mw.isAdminOrArticleAuthor, softDeleteArticle)
 
-    router.route('/:articleId([0-9a-f]{24})')
-        /**
-         * @api {put} /api/v1/articles/{articleId}
-         * @apiName updateArticle
-         * @apiVersion 1.0.0
-         * @apiGroup Article
-         * @apiPermission USER
-         * @apiDescription Is updating the requested article which is defined by :articleId mongodb _id equivalent.
-         * 
-         * @apiParam {String} [title] Article title
-         * @apiParam {String} [body] Article body
-         * @apiParam {String} [image] Image for the article
-         * @apiParam {String} token Access Token
-         * 
-         * @apiParamExample {json} Request-Example:
-         *     {
-         *       "title": "Interesting awesome title",
-         *       "body": "Here will be the body of the article",
-         *       "token": "anexampleoftoken"
-         *     }
-         * 
-         * @apiUse ArticleSuccess
-         */
-        .put(upload.single('image'), mw.isAuthenticated, mw.isAdminOrArticleAuthor, updateArticle)
-        /**
-         * @api {get} /api/v1/articles/{articleId}
-         * @apiName getArticleByArticleId
-         * @apiVersion 1.0.0
-         * @apiGroup Article
-         * @apiPermission GUEST
-         * @apiDescription Is returning the request article by articleId.
-         * 
-         * @apiSuccess {Object} article The requested article object.
-         * @apiSuccess {Boolean} success The status of the transaction.
-         */
-        .get(getArticleBy('articleId'))
-        /**
-         * @api {delete} /api/v1/articles/{articleId}
-         * @apiName softDeleteArticle
-         * @apiVersion 1.0.0
-         * @apiGroup Article
-         * @apiPermission ARTICLE_AUTHOR
-         * 
-         * @apiDescription It will softdelete the article. Article will not be seen anywhere in the system till the deleted_at field removed.
-         * 
-         * @apiSuccessExample {json} Success-Response:
-         *     HTTP/1.1 204 No Content  
-         */
-        .delete(mw.isAuthenticated, mw.isAdminOrArticleAuthor, softDeleteArticle);
+  router.route('/:slug')
+    /**
+     * @api {get} /api/v1/articles/{slug}
+     * @apiName getArticleBySlug
+     * @apiVersion 1.0.0
+     * @apiGroup Article
+     * @apiPermission GUEST
+     * @apiDescription Is returning the request article by slug.
+     *
+     * @apiSuccess {Object} article The requested article object.
+     * @apiSuccess {Boolean} success The status of the transaction.
+     */
+    .get(getArticleBy('slug'))
 
-    router.route('/:slug')
-        /**
-         * @api {get} /api/v1/articles/{slug}
-         * @apiName getArticleBySlug
-         * @apiVersion 1.0.0
-         * @apiGroup Article
-         * @apiPermission GUEST
-         * @apiDescription Is returning the request article by slug.
-         * 
-         * @apiSuccess {Object} article The requested article object.
-         * @apiSuccess {Boolean} success The status of the transaction.
-         */
-        .get(getArticleBy('slug'));
-
-    module.exports = router;
-})();
+  module.exports = router
+})()
