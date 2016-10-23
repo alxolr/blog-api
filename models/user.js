ad8bb111145e8a9c24bea846c26662b48fbfc7bb
Alexandru Olaru <alxolr@gmail.com>
2016-10-23 21:11:32 +0300

    Refactored the eslint problems in the application

--- models/user.js
+++ models/user.js
@@ -1,80 +1,75 @@
 (() => {
-    "use strict";
-    let mongoose = require('mongoose'),
-        Schema = mongoose.Schema,
-        crypto = require('crypto'),
-        utils = require('../services/utils'),
-        config = require('config'),
-        security = require('../services/security')(config);
+  'use strict'
 
-    const USER_RIGHTS = 'USER';
-    const ADMIN_RIGHTS = 'ADMIN';
+  let mongoose = require('mongoose')
+  let Schema = mongoose.Schema
+  let utils = require('../services/utils')
+  let config = require('config')
+  let security = require('../services/security')(config)
 
-    const UserSchema = new Schema({
-        'name': {
-            type: String,
-            required: false
+  const USER_RIGHTS = 'USER'
+
+  const UserSchema = new Schema({
+    'name': {
+      type: String,
+      required: false
+    },
+    'surname': {
+      type: String,
+      required: false
+    },
+    'password': {
+      type: String,
+      required: [true, utils.messages.PASSWORD_REQUIRED]
+    },
+    'email': {
+      type: String,
+      required: [true, utils.messages.EMAIL_REQUIRED],
+      validate: {
+        validator: (v, cb) => {
+          setTimeout(() => {
+            cb(/^(([^<>()\[\]\.,;:\s@"]+(\.[^<>()\[\]\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\.,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,})$/i.test(v))
+          }, 0);
         },
-        'surname': {
-            type: String,
-            required: false
-        },
-        'password': {
-            type: String,
-            required: [true, utils.messages.PASSWORD_REQUIRED]
-        },
-        'email': {
-            type: String,
-            required: [true, utils.messages.EMAIL_REQUIRED],
-            validate: {
-                validator: (v, cb) => {
-                    setTimeout(() => {
-                        cb(/^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(v));
-                    }, 0);
-                },
-                message: `{VALUE} is not a valid email address`
-            },
-            index: {
-                unique: true,
-                dropDups: true,
-            }
-        },
-        'rights': {
-            type: Array,
-            default: [USER_RIGHTS]
-        },
-        'created_at': Date,
-        'updated_at': Date,
-        'deleted_at': Date,
-        'is_subscribed': {
-            type: Boolean,
-            default: true
-        }
-    });
+        message: `{VALUE} is not a valid email address`
+      },
+      index: {
+        unique: true,
+        dropDups: true
+      }
+    },
+    'rights': {
+      type: Array,
+      default: [USER_RIGHTS]
+    },
+    'created_at': Date,
+    'updated_at': Date,
+    'deleted_at': Date,
+    'is_subscribed': {
+      type: Boolean,
+      default: true
+    }
+  })
 
+  UserSchema.pre('save', function (next) {
+    this.password = security.hashify(this.password)
 
-    UserSchema.pre('save', function(next) {
-        //hash the password 
-        this.password = security.hashify(this.password);
+    if (!this.created_at) {
+      this.created_at = new Date()
+    }
 
-        if (!this.created_at) {
-            this.created_at = new Date();
-        }
+    this.updated_at = new Date()
+    next()
+  })
 
-        this.updated_at = new Date();
-        next();
-    });
+  UserSchema.pre('update', function (next) {
+    this.update({}, {
+      $set: {
+        updated_at: new Date()
+      }
+    })
+    next()
+  })
 
-
-    UserSchema.pre('update', function(next) {
-        this.update({}, {
-            $set: {
-                updated_at: new Date()
-            }
-        });
-        next();
-    });
-
-
-    module.exports = mongoose.model('User', UserSchema);
-})();
+  module.exports = mongoose.model('User', UserSchema)
+})()
--- server.js
+++ server.js
@@ -1,42 +1,42 @@
 (() => {
-    "use strict";
+  'use strict'
 
-    let express = require('express'),
-        app = express(),
-        bodyParser = require('body-parser'),
-        mongoose = require('mongoose'),
-        assert = require('assert'),
-        userRoutes = require('./routes/user'),
-        articleRoutes = require('./routes/article'),
-        commentRoutes = require('./routes/comment'),
-        morgan = require('morgan'),
-        config = require('config');
+  let express = require('express')
+  let app = express()
+  let bodyParser = require('body-parser')
+  let mongoose = require('mongoose')
+  let assert = require('assert')
+  let userRoutes = require('./routes/user')
+  let articleRoutes = require('./routes/article')
+  let commentRoutes = require('./routes/comment')
+  let morgan = require('morgan')
+  let config = require('config')
 
-    if (process.env.NODE_ENV !== 'test') {
-        app.use(morgan('combined'));
-    }
+  if (process.env.NODE_ENV !== 'test') {
+    app.use(morgan('combined'))
+  }
 
-    mongoose.Promise = global.Promise;
-    mongoose.connect(config.database, (err) => {
-        assert.equal(err, null);
-    });
+  mongoose.Promise = global.Promise
+  mongoose.connect(config.database, (err) => {
+    assert.equal(err, null)
+  })
 
-    // parse application/x-www-form-urlencoded
-    app.use(bodyParser.urlencoded({
-        extended: false
-    }));
+  // parse application/x-www-form-urlencoded
+  app.use(bodyParser.urlencoded({
+    extended: false
+  }))
 
-    // parse application/json
-    app.use(bodyParser.json());
-    app.use('/images', express.static('images'));
-    app.use('/coverage', express.static('coverage/lcov-report/'));
-    app.use('/api/doc', express.static('doc/'));
-    
-    app.use('/api/v1/users', userRoutes);
-    app.use('/api/v1/articles', articleRoutes);
-    app.use('/api/v1/articles/', commentRoutes);
+  // parse application/json
+  app.use(bodyParser.json())
+  app.use('/images', express.static('images'))
+  app.use('/coverage', express.static('coverage/lcov-report/'))
+  app.use('/api/doc', express.static('doc/'))
 
-    app.listen(config.port);
+  app.use('/api/v1/users', userRoutes)
+  app.use('/api/v1/articles', articleRoutes)
+  app.use('/api/v1/articles/', commentRoutes)
 
-    module.exports = app;
-})();
+  app.listen(config.port)
+
+  module.exports = app
+})()
