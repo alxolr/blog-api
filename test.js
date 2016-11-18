a74a1b274f9701cf7295697d7a182569c9a3305a
Alexandru Olaru <aolaru@pentalog.fr>
2016-11-18 11:39:26 +0100

    Added user fixtures for mongo

---
+++ test.js
@@ -0,0 +1,3 @@
+let ObjectId = require('mongodb').ObjectID
+
+console.log(new ObjectId())
--- test/fixtures/users.js
+++ test/fixtures/users.js
@@ -1,7 +1,41 @@
+const config = require('config')
+const security = require('../../services/security')(config)
 const ObjectId = require('mongodb').ObjectID
 
 let users = [
   {
-    _id: new ObjectId()
+    _id: new ObjectId('582ed973861cf81df5018309'),
+    firstname: 'Alex',
+    lastname: 'Scripca',
+    email: 'ascripca@joli.com',
+    password: security.hashify('simplepassword'),
+    created_at: new Date(),
+    updated_at: new Date(),
+    is_subscribed: true,
+    rights: ['USER']
+  },
+  {
+    _id: new ObjectId('582ed9a687fdc41e6c0d9ce9'),
+    firstname: 'Jonhy',
+    lastname: 'Depp',
+    email: 'jdepp@gmail.com',
+    password: security.hashify('simplepassword'),
+    created_at: new Date(),
+    updated_at: new Date(),
+    is_subscribed: true,
+    rights: ['USER']
+  },
+  {
+    _id: new ObjectId('582ed9b47713c21ea1b04f30'),
+    firstname: 'France',
+    lastname: 'Capca',
+    email: 'fcapca@torry.com',
+    password: security.hashify('simplepassword'),
+    created_at: new Date(),
+    updated_at: new Date(),
+    is_subscribed: true,
+    rights: ['ADMIN']
   }
 ]
+
+module.exports = users
