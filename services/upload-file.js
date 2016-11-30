efa7eedaa31b96acc15ded875d92966591064bd0
Alexandru Olaru <aolaru@pentalog.fr>
2016-11-30 18:05:00 +0200

    Refactored the files to use streams

--- services/upload-file.js
+++ services/upload-file.js
@@ -1,31 +1,31 @@
-(() => {
-  'use strict'
-  const fs = require('fs')
+'use strict'
+const fs = require('fs')
 
-  /**
-   * Upload a file into a directory
-   */
-  const uploadFile = (file, directory) => {
-    return new Promise((resolve, reject) => {
-      fs.access(directory, fs.constants.F_OK, (err) => {
-        if (err) {
-          fs.mkdirSync(directory)
-        }
-        fs.readFile(file.path, function (err, data) {
+/**
+ * Upload a file into a directory
+ */
+const uploadFile = (file, directory) => {
+  // use the es6 promises to work with async data
+  return new Promise((resolve, reject) => {
+    fs.access(directory, fs.constants.F_OK, (err) => {
+      if (err) {
+        fs.mkdirSync(directory)
+      }
+
+      // using nodejs streams to gain performance
+      let readable = fs.createReadStream(file.path)
+      let writable = fs.createWriteStream(`${directory}/${file.originalname}`)
+      readable.pipe(writable)
+
+      // attach the end event to remove the initial file
+      readable.on('end', () => {
+        fs.unlink(file.path, (err) => {
           if (err) reject(err)
-
-          fs.writeFile(`${directory}/${file.originalname}`, data, function (err) {
-            if (err) reject(err)
-
-            fs.unlink(file.path, (err) => {
-              if (err) reject(err)
-              resolve()
-            })
-          })
+          resolve()
         })
       })
     })
-  }
+  })
+}
 
-  module.exports = uploadFile
-})()
+module.exports = uploadFile
