f13a29d2190e9ad63e138b4550ea7e7a6d634bba
Alexandru Olaru <alxolr@gmail.com>
2017-01-03 21:13:13 +0200

    Added voca to the stack

--- services/utils.js
+++ services/utils.js
@@ -1,44 +1,42 @@
-(() => {
-  'use strict'
+'use strict'
 
-  module.exports = {
-    listifyErrors: (err) => {
-      let errorMessages = []
-      for (let property in err.errors) {
-        errorMessages.push({
-          'property': property,
-          'message': err.errors[property].message
-        })
-      }
+const voca = require('voca')
 
-      if (err.hasOwnProperty('message')) {
-        errorMessages.push(err.message)
-      }
-
-      return errorMessages
-    },
-
-    slugify: text => {
-      return text.toLowerCase()
-        .replace(/ /g, '-')
-        .replace(/[^\w-]+/g, '')
-    },
-
-    mongo: {
-      UNIQUE_KEY_VIOLATION: 11000
-    },
-    messages: {
-      PASSWORD_REQUIRED: 'User should provide a password.',
-      EMAIL_REQUIRED: 'User should provide a valid email address.',
-      INVALID_CREDENTIALS: 'The provided email and password does not match.',
-      USER_DUPLICATE: 'User already registered in the system.',
-      USER_NOT_FOUND: 'Requested user not found.',
-      TOKEN_NOT_PROVIDED: 'Access denied. Please provide a token.',
-      TOKEN_EXPIRED: 'Access denied. Provided token or invalid or expired',
-      TOKEN_HIGHJACKED: 'Access denied. Provided token was highjacked',
-      MONGOID_INVALID: 'Provided ID is not a valid one.',
-      ARTICLE_NOT_FOUND: 'Requested article was not found',
-      COMMENT_CREATE_FAIL: 'The comment was not been succesfull'
+module.exports = {
+  listifyErrors: (err) => {
+    let errorMessages = []
+    for (let property in err.errors) {
+      errorMessages.push({
+        'property': property,
+        'message': err.errors[property].message
+      })
     }
+
+    if (err.hasOwnProperty('message')) {
+      errorMessages.push(err.message)
+    }
+
+    return errorMessages
+  },
+
+  slugify: text => {
+    return voca.slugify(text)
+  },
+
+  mongo: {
+    UNIQUE_KEY_VIOLATION: 11000
+  },
+  messages: {
+    PASSWORD_REQUIRED: 'User should provide a password.',
+    EMAIL_REQUIRED: 'User should provide a valid email address.',
+    INVALID_CREDENTIALS: 'The provided email and password does not match.',
+    USER_DUPLICATE: 'User already registered in the system.',
+    USER_NOT_FOUND: 'Requested user not found.',
+    TOKEN_NOT_PROVIDED: 'Access denied. Please provide a token.',
+    TOKEN_EXPIRED: 'Access denied. Provided token or invalid or expired',
+    TOKEN_HIGHJACKED: 'Access denied. Provided token was highjacked',
+    MONGOID_INVALID: 'Provided ID is not a valid one.',
+    ARTICLE_NOT_FOUND: 'Requested article was not found',
+    COMMENT_CREATE_FAIL: 'The comment was not been succesfull'
   }
-})()
+}
