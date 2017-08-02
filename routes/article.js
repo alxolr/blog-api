56a88bc9549b5569c45aa5188f722ebdeac35000
Alexandru Olaru <alxolr@gmail.com>
2017-08-02 17:46:21 +0300

    Refactored the article routes

--- routes/article.js
+++ routes/article.js
@@ -1,267 +1,180 @@
-'use strict'
+const router = require('express').Router();
+const mw = require('../middlewares/middlewares');
+const Article = require('../models/article');
+const utils = require('../services/utils');
+const multer = require('multer');
+const path = require('path');
+const fe = require('../services/filter-extractor');
+const qb = require('../services/query-builder');
+const config = require('config');
+const uploadFile = require('../services/upload-file');
 
-const router = require('express').Router()
-const mw = require('../middlewares/middlewares')
-const Article = require('../models/article')
-const utils = require('../services/utils')
-const multer = require('multer')
-const path = require('path')
-const fe = require('../services/filter-extractor')
-const qb = require('../services/query-builder')
-const config = require('config')
-const uploadFile = require('../services/upload-file')
 const upload = multer({
-  dest: '/tmp/'
-})
-const imgDirectory = path.resolve('images/')
+  dest: '/tmp/',
+});
+const imgDirectory = path.resolve('images/');
 
-module.exports = router
+module.exports = router;
 
-router.get('', findArticles)
 
-/**
- * @apiDefine ArticleSuccess
- *
- * @apiSuccess {Boolean} success Returns if the article was succesfully created.
- * @apiSuccess {Object} article Returns the newly created article
- * @apiSuccess {String} message Returns the success message.
- */
+function handleErrors(res) {
+  return (err) => {
+    res.status(400).json({
+      error: utils.listifyErrors(err),
+    });
+  };
+}
 
-/**
- * @api {post} /api/v1/articles
- * @apiName createArticle
- * @apiVersion 1.0.0
- * @apiGroup Article
- * @apiPermission USER
- *
- * @apiParam {String} title Article title
- * @apiParam {String} body Article body
- * @apiParam {String} [image] Image for the article
- *
- * @apiParamExample {json} Request-Example:
- *     {
- *       "title": "Interesting awesome title",
- *       "body": "Here will be the body of the article",
- *       "token": "anexampleoftoken"
- *     }
-  *@apiHeader (Headers) {String} Authorization Bearer token.
- * @apiHeaderExample {json} Header-Example:
- *     {
- *       "Authorization": "Bearer token"
- *     }
- *
- * @apiUse ArticleSuccess
- */
-router.post('/', upload.single('image'), mw.isAuthenticated, createArticle)
+function saveArticle(article, res) {
+  article.save((err) => {
+    if (err) {
+      return handleErrors(res)(err);
+    }
+    return res.json(article);
+  });
+}
 
-router.route('/:articleId([0-9a-f]{24})')
-  /**
-   * @api {put} /api/v1/articles/{articleId}
-   * @apiName updateArticle
-   * @apiVersion 0.0.1
-   * @apiGroup Article
-   * @apiPermission USER
-   * @apiDescription Is updating the requested article which is defined by :articleId mongodb _id equivalent.
-   *
-   * @apiParam {String} [title] Article title
-   * @apiParam {String} [body] Article body
-   * @apiParam {String} [image] Image for the article
-   * @apiParam {String} token Access Token
-   *
-   * @apiParamExample {json} Request-Example:
-   *     {
-   *       "title": "Interesting awesome title",
-   *       "body": "Here will be the body of the article",
-   *       "token": "anexampleoftoken"
-   *     }
-   *
-   * @apiUse ArticleSuccess
-   */
-  .put(upload.single('image'), mw.isAuthenticated, mw.isAdminOrArticleAuthor, updateArticle)
-  /**
-   * @api {get} /api/v1/articles/{articleId}
-   * @apiName getArticleByArticleId
-   * @apiVersion 0.0.1
-   * @apiGroup Article
-   * @apiPermission GUEST
-   * @apiDescription Is returning the request article by articleId.
-   *
-   * @apiSuccess {Object} article The requested article object.
-   * @apiSuccess {Boolean} success The status of the transaction.
-   */
-  .get(getArticleByProperty('articleId'))
-  /**
-   * @api {delete} /api/v1/articles/{articleId}
-   * @apiName softDeleteArticle
-   * @apiVersion 0.0.1
-   * @apiGroup Article
-   * @apiPermission ARTICLE_AUTHOR
-   *
-   * @apiDescription It will softdelete the article. Article will not be seen anywhere in the system till the deleted_at field removed.
-   *
-   * @apiSuccessExample {json} Success-Response:
-   *     HTTP/1.1 204 No Content
-   */
-  .delete(mw.isAuthenticated, mw.isAdminOrArticleAuthor, softDeleteArticle)
 
-router.route('/:slug')
-  /**
-   * @api {get} /api/v1/articles/{slug}
-   * @apiName getArticleBySlug
-   * @apiVersion 0.0.1
-   * @apiGroup Article
-   * @apiPermission GUEST
-   * @apiDescription Is returning the request article by slug.
-   *
-   * @apiSuccess {Object} article The requested article object.
-   * @apiSuccess {Boolean} success The status of the transaction.
-   */
-  .get(getArticleByProperty('slug'))
-
-function createArticle (req, res) {
-  let article = new Article(req.body)
-
+function createArticle(req, res) {
+  const article = new Article(req.body);
   article.author = {
     _id: req.decoded._id,
     name: req.decoded.firstname,
-    surname: req.decoded.lastname
-  }
+    surname: req.decoded.lastname,
+  };
 
   if (req.file !== undefined) {
     uploadFile(req.file, imgDirectory).then(() => {
-      article.image = `/images/${req.file.originalname}`
-      saveArticle(article, res)
-    }).catch(handleErrors(res))
+      article.image = `/images/${req.file.originalname}`;
+      saveArticle(article, res);
+    }).catch(handleErrors(res));
   } else {
-    saveArticle(article, res)
+    saveArticle(article, res);
   }
 }
 
-function saveArticle (article, res) {
-  article.save((err) => {
-    if (err) {
-      return handleErrors(res)(err)
-    } else {
-      return res.json(article)
-    }
-  })
-}
-
-function handleErrors (res) {
-  return function (err) {
-    res.status(400).json({
-      error: utils.listifyErrors(err)
-    })
-  }
-}
-
-function getArticleByProperty (property) {
-  return function (req, res) {
-    let query = {
+function getArticleByProperty(property) {
+  return (req, res) => {
+    const query = {
       deleted_at: {
-        '$exists': false
-      }
-    }
+        $exists: false,
+      },
+    };
 
     if (property === 'slug') {
-      query.slug = req.params[property]
+      query.slug = req.params[property];
     } else {
-      query._id = req.params[property]
+      query._id = req.params[property];
     }
 
     Article
       .findOne(query)
       .then((article) => {
         if (article) {
-          res.json(article)
+          res.json(article);
         } else {
           res.status(404).json({
-            error: utils.messages.ARTICLE_NOT_FOUND
-          })
+            error: utils.messages.ARTICLE_NOT_FOUND,
+          });
         }
       })
-      .catch(handleErrors(res))
-  }
+      .catch(handleErrors(res));
+  };
 }
 
-function updateArticle (req, res) {
+function updateArticle(req, res) {
+  function handleSuccess() {
+    Article.findOne({
+      _id: req.params.articleId,
+    })
+      .then(article => res.json(article))
+      .catch(handleErrors(res));
+  }
   if (req.body.title !== undefined) {
-    req.body.slug = utils.slugify(req.body.title)
+    req.body.slug = utils.slugify(req.body.title);
   }
 
-  if (req.body._id !== undefined) delete req.body._id
+  if (req.body._id !== undefined) delete req.body._id;
 
   if (req.file !== undefined) {
     uploadFile(req.file, imgDirectory).then(() => {
-      req.body.image = `/images/${req.file.originalname}`
-      Article.update({
-        _id: req.params.articleId
-      }, {
-        '$set': req.body
-      }).then(handleSuccess, handleErrors(res))
+      req.body.image = `/images/${req.file.originalname}`;
+      Article.update(
+        {
+          _id: req.params.articleId,
+        },
+        {
+          $set: req.body,
+        }).then(handleSuccess, handleErrors(res));
     })
-      .catch(handleErrors(res))
+      .catch(handleErrors(res));
   } else {
-    Article.update({
-      _id: req.params.articleId
-    }, {
-      '$set': req.body
-    }).then(handleSuccess)
-      .catch(handleErrors(res))
-  }
-
-  function handleSuccess (result) {
-    Article.findOne({
-      _id: req.params.articleId
-    })
-      .then(article => res.json(article))
-      .catch(handleErrors(res))
+    Article.update(
+      {
+        _id: req.params.articleId,
+      },
+      {
+        $set: req.body,
+      }).then(handleSuccess)
+      .catch(handleErrors(res));
   }
 }
 
-function softDeleteArticle (req, res) {
+function softDeleteArticle(req, res) {
   Article
     .update({
-      _id: req.params.articleId
+      _id: req.params.articleId,
     }, {
       $set: {
-        deleted_at: new Date()
-      }
+        deleted_at: new Date(),
+      },
     })
-    .then((result) => {
+    .then(() => {
       // 204 No Content
-      res.status(204).end()
+      res.status(204).end();
     })
-    .catch(handleErrors(res))
+    .catch(handleErrors(res));
 }
 
-function findArticles (req, res) {
-  let filter = req.query.filter || null
-  let filters = fe.extract(filter)
-  let limit = parseInt(req.query.limit) || config.perPage
-  let skip = parseInt(req.query.skip) || 0
-  let query = qb.build(filters)
+function findArticles(req, res) {
+  const filter = req.query.filter || null;
+  const filters = fe.extract(filter);
+  const limit = parseInt(req.query.limit) || config.perPage;
+  const skip = parseInt(req.query.skip) || 0;
+  const query = qb.build(filters);
 
   query.deleted_at = {
-    '$exists': false
-  }
+    $exists: false,
+  };
 
   Article.find(query).count().lean().exec((err, count) => {
-    if (err) handleErrors(res)(404)
-    let results = Article.find(query).sort({'updated_at': -1})
-    results.limit(limit)
-    results.skip(skip)
+    if (err) handleErrors(res)(404);
+    const results = Article.find(query).sort({ updated_at: -1 });
+    results.limit(limit);
+    results.skip(skip);
 
     results.lean().exec((err, articles) => {
-      if (err) handleErrors(res)(404)
-      let response = {
-        count: count,
-        limit: limit,
-        skip: skip,
-        articles: articles
-      }
+      if (err) handleErrors(res)(404);
+      const response = {
+        count,
+        limit,
+        skip,
+        articles,
+      };
 
-      return res.json(response)
-    })
-  })
+      return res.json(response);
+    });
+  });
 }
+
+
+router.get('', findArticles);
+router.post('/', upload.single('image'), mw.isAuthenticated, createArticle);
+
+router.route('/:articleId([0-9a-f]{24})')
+  .put(upload.single('image'), mw.isAuthenticated, mw.isAdminOrArticleAuthor, updateArticle)
+  .get(getArticleByProperty('articleId'))
+  .delete(mw.isAuthenticated, mw.isAdminOrArticleAuthor, softDeleteArticle);
+
+router.route('/:slug')
+  .get(getArticleByProperty('slug'));
