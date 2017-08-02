2c9ff13c080ca94df494c22b8cd389260ef9886e
Alexandru Olaru <alxolr@gmail.com>
2017-08-02 17:29:22 +0300

    Added arbnb eslint rule

--- bin/fixtures-load.js
+++ bin/fixtures-load.js
@@ -1,80 +1,78 @@
-'use strict'
+const MongoClient = require('mongodb').MongoClient;
+const async = require('async');
+const fixtures = require('../test/fixtures');
+const config = require('config');
+const assert = require('assert');
 
-const MongoClient = require('mongodb').MongoClient
-const async = require('async')
-const fixtures = require('../test/fixtures')
-const config = require('config')
-const assert = require('assert')
-
-function log (text) {
-  console.log(text)
+function log(text) {
+  console.log(text);
 }
 
 MongoClient.connect(config.database, (err, db) => {
-  assert.equal(err, null)
-  log(`Succesfully connected to ${config.database}`)
+  assert.equal(err, null);
+  log(`Succesfully connected to ${config.database}`);
 
-  function dropUsers (callback) {
-    db.collection('users').remove({}, (err, result) => {
-      assert.equal(err, null)
-      log('Collection users succesfully removed')
+  function dropUsers(callback) {
+    db.collection('users').remove({}, (err) => {
+      assert.equal(err, null);
+      log('Collection users succesfully removed');
 
-      callback(null)
-    })
+      callback(null);
+    });
   }
 
-  function dropArticles (callback) {
-    db.collection('articles').remove({}, (err, result) => {
-      assert.equal(err, null)
-      log('Collection articles succesfully removed')
-      callback(null)
-    })
+  function dropArticles(callback) {
+    db.collection('articles').remove({}, (err) => {
+      assert.equal(err, null);
+      log('Collection articles succesfully removed');
+      callback(null);
+    });
   }
 
-  function insertUser (user, cb) {
+  function insertUser(user, cb) {
     db.collection('users').insertOne(user, (err) => {
-      assert.equal(err, null)
-      cb()
-    })
+      assert.equal(err, null);
+      cb();
+    });
   }
 
-  function insertArticle (article, cb) {
+  function insertArticle(article, cb) {
     db.collection('articles').insertOne(article, (err) => {
-      assert.equal(err, null)
-      cb()
-    })
+      assert.equal(err, null);
+      cb();
+    });
   }
 
   async.parallel([
     dropUsers,
-    dropArticles
+    dropArticles,
   ],
-    (err, results) => {
-      assert.equal(err, null)
-      function insertUsers (cb) {
-        async.map(fixtures.users, insertUser, (err, result) => {
-          assert.equal(err, null)
-          log('Users collection was updated with ' + fixtures.users.length + ' documents')
-          cb(null)
-        })
+    (err) => {
+      assert.equal(err, null);
+      function insertUsers(cb) {
+        async.map(fixtures.users, insertUser, (err) => {
+          assert.equal(err, null);
+          log(`Users collection was updated with ${fixtures.users.length} documents`);
+          cb(null);
+        });
       }
-      function insertArticles (cb) {
-        async.map(fixtures.articles, insertArticle, (err, result) => {
-          assert.equal(err, null)
-          log('Article collection was updated with ' + fixtures.articles.length + ' documents')
-          cb(null)
-        })
+      function insertArticles(cb) {
+        async.map(fixtures.articles, insertArticle, (err) => {
+          assert.equal(err, null);
+          log(`Article collection was updated with ${fixtures.articles.length} documents`);
+          cb(null);
+        });
       }
 
       async.parallel([
         insertUsers,
-        insertArticles
-      ], (err, res) => {
-        assert.equal(err, null)
-        log('Fixtures was succesfully loaded')
+        insertArticles,
+      ], (err) => {
+        assert.equal(err, null);
+        log('Fixtures was succesfully loaded');
 
-        db.close()
-      })
-    }
-  )
-})
+        db.close();
+      });
+    },
+  );
+});
